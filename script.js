d0fc5f77234fc26587e44d0b398bd597cb2eb4db
refandii <indrarefandi52@gmail.com>
2025-05-03 19:28:20 +0700

    seven commit

--- script.js
+++ script.js
@@ -33,3 +33,25 @@ const navList = document.querySelector('.nav-list');
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+
+
+// navbar transition
+const navbar = document.getElementsByTagName('nav')[0];
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY);
+    if (window.scrollY > 1) {
+        navbar.classList.replace('navbar', 'navbar-scroll');
+    } else if (this.window.scrollY <= 0) {
+        navbar.classList.replace('navbar-scroll', 'navbar');
+    }
+});
+
+const navlist = document.getElementsByTagName('ul')[0];
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY);
+    if (window.scrollY > 1) {
+        navlist.classList.replace('nav-list-scroll', 'nav-list');
+    } else if (this.window.scrollY <= 0) {
+        navlist.classList.replace('nav-list', 'nav-list-scroll');
+    }
+});
